8a92ae060cdab7ea258aa34a81dba419d537235f
Ze8c <Ze8c.Max@gmail.com>
2024-05-03 22:50:56 +0200

    Add serialize methods

---
+++ Sources/Entities/CountsDTO.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  
+//
+//  Created by Maksym Sytyi on 5/2/24.
+//
+
+struct CountDTO: Codable {
+    let likes: Int
+    let views: Int
+}
---
+++ Sources/Entities/OwnerDTO.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Maksym Sytyi on 5/2/24.
+//
+
+import Foundation
